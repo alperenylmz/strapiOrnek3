7b4ef679a7ffaf925e7ad312bfe1dd61ef8188ef
alperenylmz <emiralperenylmz@outlook.com>
2024-11-09 01:00:47 +0300

    laaaatest

--- config/plugins.js
+++ config/plugins.js
@@ -21,16 +21,4 @@ module.exports = ({ env }) => ({
     enabled: true,
     resolve: "./src/plugins/subscribers",
   },
-  
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_V6788XvajXzMBSLSIXkMGZLQuw18/G8ZTJkW1I2",
-      apiToken: "4OMSZpJJ35pAUuGeierkfKx7",
-      //appFilter: "your-app-name-on-vercel",
-      //teamFilter: "your-team-id-on-vercel",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
-  },
 });
